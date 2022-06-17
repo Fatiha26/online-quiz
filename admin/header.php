b59cb20cdf73f0665d37f8be0c88285e04f7bc74
LamiaFatiha <f6lamia@gmail.com>
2022-06-17 08:41:04 +0600

    title and other changes

--- admin/header.php
+++ admin/header.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Admin Panel</title>
+    <title>QuizClan | Admin Panel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="vendors/bootstrap/dist/css/bootstrap.min.css">
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Quiz</title>
+    <title>QuizClan</title>
     <link rel="stylesheet" href="css1/bootstrap.min.css">
     <link rel="stylesheet" href="css1/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -27,7 +27,7 @@
                     <nav>
                         <a href="index1.php" style="text-decoration: none;"><i class="fi fi-sr-home"></i>Home</a>
                         <a href="#about" style="text-decoration: none;">About</a>
-                        <a href="#education" style="text-decoration: none;">Education</a>
+                        <a href="https://www.w3schools.com/php/default.asp" style="text-decoration: none;">Education</a>
                         <a href="#contact" style="text-decoration: none;">Contact</a>
 
                     </nav>
@@ -98,6 +98,7 @@
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                          Dolorem rerum delectus, nemo neque ullam dolorum?
                     </p>
+                    <a href="login.php" class="btn1">Participate Now</a>
                    </div>
                 </div>
             </div>
